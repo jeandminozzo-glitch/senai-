ca59f06263fe3eb98fdcc85ceeb89c669b332a91
jeandminozzo-glitch <jeandminozzo@gmail.com>
2026-07-15 20:59:23 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,54 +1,54 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter import messagebox 
+from tkinter import messagebox
 janela = tk.Tk()
-janela.title("Sistema de login ")
+janela.title("Sistema de Login ")
 janela.geometry("350x200")
 
 def login():
     usuario = edt_login.get()
     senha = edt_senha.get()
 
-    if usuario == "Jean" and senha == "123":
-        messagebox("Login realizado com sucesso!")
-    else: 
-        messagebox.showerror("Erro", "usuário ou senha inválidos!")
+    if usuario == "Fudencio" and senha == "123":
+        messagebox.showinfo("Login","Login realizado com sucesso!")
+    else:
+        messagebox.showerror("Erro", "Usuário ou senha inválidos!")
 
 lbl_login = ttk.Label(
-    janela, 
+    janela,
     text="Login:"
 )
 lbl_login.grid(
-    row=0, 
-    colum=0,
+    row=0,
+    column=0,
     padx=10,
     pady=10
 )
 edt_login = ttk.Entry(
     janela,
-    witdh=25
+    width=25
 )
 edt_login.grid(
     row=0,
     column=1,
     padx=10,
     pady=10
-)
+)    
 
-############################
+#########################
 lbl_senha = ttk.Label(
-    janela, 
-    text="Login:"
+    janela,
+    text="Senha:"
 )
 lbl_senha.grid(
-    row=1, 
-    colum=0,
+    row=1,
+    column=0,
     padx=10,
     pady=10
 )
 edt_senha = ttk.Entry(
     janela,
-    witdh=25,
+    width=25,
     show="*"
 )
 edt_senha.grid(
@@ -56,10 +56,19 @@ edt_senha.grid(
     column=1,
     padx=10,
     pady=10
+)       
+
+# Criar o botão de login
+btn_login = ttk.Button(
+    janela,
+    text = "Entrar",
+    command=login
 )
-
+btn_login.grid(
+    row=2,
+    column=0,
+    columnspan=2
+)
 janela.mainloop()
-
-
 
 
